9f4301116c2c39a9d1723ce64af4e7ed6843b970
jadone237 <martinfouepi@gmail.com>
2025-09-30 08:32:25 +0200

    ajout de l'entité reservation

--- src/main/java/com/gestionagence/entities/Reservation.java
+++ src/main/java/com/gestionagence/entities/Reservation.java
@@ -1,4 +1,28 @@
 package com.gestionagence.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "reservations")
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nomClient;
+    private String emailClient;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateReservation;
+
+    private String statut;
 }
