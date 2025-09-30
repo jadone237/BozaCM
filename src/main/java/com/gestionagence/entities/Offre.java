eb20c4cb89688b2eb36dbe89a66ceba0a70ca969
jadone237 <martinfouepi@gmail.com>
2025-09-30 08:29:37 +0200

    ajout de l'entité offre

--- src/main/java/com/gestionagence/entities/Offre.java
+++ src/main/java/com/gestionagence/entities/Offre.java
@@ -1,4 +1,27 @@
 package com.gestionagence.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "offres")
 public class Offre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String titre;
+    private String description;
+    private Double prix;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateDepart;
 }
